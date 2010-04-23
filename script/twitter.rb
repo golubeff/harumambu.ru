1f310aced3828bfd04df23306c3c3d3442af17f8
Голубев Павел <golubeff@MacBook-Pro.local>
2010-04-23 20:46:04 +0400

    twitter fixes

--- script/twitter.rb
+++ script/twitter.rb
@@ -3,13 +3,13 @@ require 'sinatra'
 require 'twitter_oauth'
 
 client = TwitterOAuth::Client.new(:consumer_key => 'bDbB4QgQtwFKxETUfqg8LQ', :consumer_secret => 'd4l4cBj5yzfuACQVLo0KsPoaU1PHlAAcyyHoUWEM')
-request_token = client.request_token(:oauth_callback => 'http://harumambu.ru/twitter/connected')
+request_token = client.request_token(:oauth_callback => 'http://harumambu.ru/connected_twitter')
 
 get '/twitter' do
   redirect request_token.authorize_url
 end
 
-get '/twitter/connected' do
+get '/connected_twitter' do
   client.authorize(
     request_token.token,
     request_token.secret,
