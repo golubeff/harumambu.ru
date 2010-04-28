cb82260b6480d576b71eebe227cbf2ab93b65e9a
Голубев Павел <golubeff@MacBook-Pro.local>
2010-04-28 21:26:25 +0400

    db:seed

--- db/migrate/20100428171337_set_default_project_category.rb
+++ db/migrate/20100428171337_set_default_project_category.rb
@@ -1,5 +1,6 @@
 class SetDefaultProjectCategory < ActiveRecord::Migration
   def self.up
+    `rake db:seed`
     Project.update_all "category_id = #{Category.others.id}", "category_id is null"
     execute "alter table projects alter column category_id set not null"
   end
